4bff86270825831a8c7929d1ae625d3654141c43
Seoyeon Lee <90055686+olsy1128@users.noreply.github.com>
2021-09-16 23:49:37 +0900

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	int num1, num2;
-	printf("µÎ Á¤¼ö¸¦ ÀÔ·ÂÇÏ¼¼¿ä.: ");
+	printf("ë‘ ì •ìˆ˜ë¥¼ ì…ë ¥í•˜ì„¸ìš”: ");
 	scanf("%d %d", &num1, &num2);
 
 	printf("%d & %d = %d \n", num1, num2, num1 & num2);
