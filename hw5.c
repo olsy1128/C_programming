e35628e3ced24b6bb3a6d7f858ec5c62e371bc0a
olsy1128 <lsy021128@naver.com>
2021-11-09 16:23:58 +0900

    hw5_C-programming

---
+++ hw5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int even(int arr)
+{
+    if (arr % 2 == 0)
+        printf("%d ", arr);
+    return 0;
+}
+
+int odd(int arr)
+{
+    if (arr % 2 == 1)
+        printf("%d ", arr);
+    return 0;
+}
+
+int main()
+{
+    int arr[5];
+    int i = 0;
+
+    printf("5개의 정수 입력: ");
+    
+    while (i < 5)
+    {
+        scanf("%d", &arr[i]);
+        i++;
+    }
+
+    printf("\n");
+
+    printf("홀수 출력: ");
+    for (i = 0; i < 5; i++)
+    {
+        odd(arr[i]);    
+    }
+
+    printf("\n");
+
+    printf("짝수 출력: ");
+    for (i = 0; i < 5; i++)
+    {
+        even(arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
